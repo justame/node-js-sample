3e89eebb4c21be12d4dfcf50f5eb8087c14c0bdf
Yaron Nachshon <yaronn@wix.com>
2016-09-23 18:10:07 +0300

    record

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function conf(response){
   };
 
   var conference_name = "demo"; // Conference Room name
-  r.addRecord(conference_name, params);
+  r.addRecord();
   response.end(r.toXML());
 }
 app.listen(app.get('port'), function() {
